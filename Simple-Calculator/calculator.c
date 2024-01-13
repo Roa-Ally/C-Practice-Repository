f221405822a808897a4569998005e5c39fb5f481
Ayleen Roa <roa.ayleen07@gmail.com>
2024-01-12 19:45:24 -0500

    print statment works

---
+++ Simple-Calculator/calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(int arg, char *argv[]){
+	float var1;
+	float var2;
+	char oper;
+	float ans;
+
+	printf("Hello please enter a number followed by an operator followed by a number. Please select enter when done");
+}	
