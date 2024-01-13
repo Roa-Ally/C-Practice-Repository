c026557504fcbe35527fee34daadb41b46d4ca97
Ayleen Roa <roa.ayleen07@gmail.com>
2024-01-12 20:35:54 -0500

    scanner workd finished calculator with +,_,*,/ functions

--- Simple-Calculator/calculator.c
+++ Simple-Calculator/calculator.c
@@ -1,13 +1,35 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-int main(int arg, char *argv[]){
+int main(int argc, char *arvg[]){
 	float var1;
 	float var2;
-	char oper;
-	float ans;
+	char operator;
+	float answ;
 
-	printf("Hello please enter a number followed by an operator followed by a number. Please select enter when done");
-}	
+	printf("Please enter a number followed by an operator followed by another number: \n\n");
+	fflush(stdout);
+	scanf("%f %c %f", &var1, &operator, &var2);
+
+	switch(operator){
+		case '+': answ = var1 + var2;
+			  break;
+		case '-': answ = var1 - var2;
+			  break;
+		case '*': answ = var1 * var2;
+			  break;
+		case '/': answ = var1 / var2;
+			  break;
+		default: goto fail;
+	}
+	printf("%.9g%c%.9g =  %.6g\n\n", var1, operator, var2, answ);
+	goto exit;
+
+fail:
+	printf("invalid input please try again in the proper format number followed by operator followed by a number");
+exit:
+	return 0;
+}
